59e3e8e1a906c3e384ebcc4eb3eaa1674d00c5f5
Tzu-ping Chung <uranusjr@gmail.com>
2018-05-27 16:40:50 +0900

    Fix version.txt inclusion

--- tasks.py
+++ tasks.py
@@ -1,8 +1,16 @@
-import sys
-
 import invoke
 
 
 @invoke.task()
 def build(ctx):
-    ctx.run(f'"{sys.executable}" setup.py sdist bdist_wheel')
+    ctx.run(f'python setup.py sdist bdist_wheel')
+
+
+@invoke.task()
+def clean(ctx):
+    ctx.run(f'python setup.py clean')
+
+
+@invoke.task(pre=[clean, build])
+def upload(ctx, repo):
+    ctx.run(f'twine upload --repository="{repo}" dist/*')
