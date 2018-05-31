41d3f6f48651bba3918520f1b08690fcbb0ce10a
Tzu-ping Chung <uranusjr@gmail.com>
2018-05-31 11:22:56 +0800

    Comments

--- tasks.py
+++ tasks.py
@@ -3,14 +3,27 @@ import invoke
 
 @invoke.task()
 def build(ctx):
+    """Build the package into distributables.
+
+    This will create two distributables: source and wheel.
+    """
     ctx.run(f'python setup.py sdist bdist_wheel')
 
 
 @invoke.task()
 def clean(ctx):
+    """Clean previously built package artifacts.
+    """
     ctx.run(f'python setup.py clean')
 
 
 @invoke.task(pre=[clean, build])
 def upload(ctx, repo):
+    """Upload the package to an index server.
+
+    This implies cleaning and re-building the package.
+
+    :param repo: Required. Name of the index server to upload to, as specifies
+        in your .pypirc configuration file.
+    """
     ctx.run(f'twine upload --repository="{repo}" dist/*')
